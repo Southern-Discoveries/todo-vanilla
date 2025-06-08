aff45f85d37d24edb15bd909c3a9627b01cbe256
synasapmob <sonn51280@gmail.com>
2025-06-08 14:29:07 +0700

    update database connection

--- src/utils/utils.pg.ts
+++ src/utils/utils.pg.ts
@@ -1,10 +1,10 @@
 import { Client } from "pg";
 
 const clientPG = new Client({
-  user: "postgres",
-  password: "admin",
-  host: "localhost",
-  database: "postgres",
+  user: "todo_postgresql_sugt_user",
+  password: "nW9BOrLVJigqmyez6F8ir3gh8YbYs2m0",
+  host: "dpg-d12ji33e5dus73cietjg-a",
+  database: "todo_postgresql_sugt",
   port: 5432,
 });
 
