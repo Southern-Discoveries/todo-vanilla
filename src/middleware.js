b29398a75c794934eeb8bd59e33026fea89795d5
synasapmob <sonn51280@gmail.com>
2025-06-09 14:27:24 +0700

    mistake pathname

--- src/middleware.js
+++ src/middleware.js
@@ -4,16 +4,17 @@ import utilsConstants from "../utils/utils.constants";
 $.ready(
   (function () {
     const getAccount = localStorage.getItem(utilsConstants.STORAGE_ACCOUNT);
-    const isLoggedPage = location.pathname === "/login";
+
+    const isLoggedPage = window.location.href.includes("login");
 
     // handler you're logged or not?
     {
       if (!getAccount?.length && !isLoggedPage) {
-        window.location = `${utilsConstants.BASE_PATH}/login`;
+        window.location.pathname = `${utilsConstants.BASE_PATH}/login`;
       }
 
       if (getAccount?.length && isLoggedPage) {
-        window.location = `${utilsConstants.BASE_PATH}/`;
+        window.location.pathname = `${utilsConstants.BASE_PATH}/`;
       }
     }
 
