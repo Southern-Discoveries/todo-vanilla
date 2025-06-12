5e87163396a57a950b5349847d94139c31d300b6
synasapmob <sonn51280@gmail.com>
2025-06-12 18:37:50 +0700

    omit constants

--- utils/utils.constants.js
+++ utils/utils.constants.js
@@ -3,8 +3,13 @@ const STORAGE_ACCOUNT = "account";
 const GET_API = "https://todo-vanilla-sb13.onrender.com";
 const BASE_PATH = process.env.NODE_ENV === "production" ? "todo-vanilla" : "";
 
+const ACCESS_TOKEN = "ACCESS_TOKEN";
+const REFRESH_TOKEN = "REFRESH_TOKEN";
+
 export default {
   STORAGE_ACCOUNT,
   GET_API,
   BASE_PATH,
+  ACCESS_TOKEN,
+  REFRESH_TOKEN,
 };
