aa51cc0ea30fc2c329d8ab05f5c7e2c467f1eb38
synasapmob <sonn51280@gmail.com>
2025-06-12 18:27:40 +0700

    needs use access token

--- src/middleware.js
+++ src/middleware.js
@@ -1,16 +1,58 @@
 import $ from "jquery";
 import utilsConstants from "../utils/utils.constants";
+import { parseJWT } from "../utils";
+
+let getAccessToken = localStorage.getItem(utilsConstants.ACCESS_TOKEN);
+let getRefreshToken = localStorage.getItem(utilsConstants.REFRESH_TOKEN);
 
 let cache_account = null;
 
 export const fetchAccount = async () => {
-  const username = localStorage.getItem(utilsConstants.STORAGE_ACCOUNT);
+  if (!getAccessToken?.length) return null;
 
-  if (!username?.length) return null;
+  const parseAccessToken = parseJWT(getAccessToken);
+  const parseRefreshToken = parseJWT(getRefreshToken);
+
+  // expires refresh token
+  if (parseRefreshToken.exp * 1000 < Date.now()) {
+    localStorage.removeItem(utilsConstants.ACCESS_TOKEN);
+    localStorage.removeItem(utilsConstants.REFRESH_TOKEN);
+
+    window.location.pathname = `${utilsConstants.BASE_PATH}/login`;
+  }
+
+  // expires access token
+  if (parseAccessToken.exp * 1000 < Date.now()) {
+    const request = await fetch(
+      `${utilsConstants.GET_API}/user/refresh-token`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${getRefreshToken}`,
+        },
+      }
+    );
+
+    const json = await request.json();
+
+    cache_account = undefined;
+
+    getRefreshToken = json.REFRESH_TOKEN;
+    getAccessToken = json.ACCESS_TOKEN;
+
+    localStorage.setItem(utilsConstants.ACCESS_TOKEN, getAccessToken);
+    localStorage.setItem(utilsConstants.REFRESH_TOKEN, getRefreshToken);
+  }
 
   if (!cache_account) {
     const request_account = await fetch(
-      `${utilsConstants.GET_API}/user/profile/${username}`
+      `${utilsConstants.GET_API}/user/profile`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${getAccessToken}`,
+        },
+      }
     );
 
     cache_account = await request_account.json();
@@ -21,30 +63,18 @@ export const fetchAccount = async () => {
 
 $.ready(
   (async function () {
-    const getAccount = localStorage.getItem(utilsConstants.STORAGE_ACCOUNT);
-
     const isLoggedPage =
       window.location.href.includes("login") ||
       window.location.href.includes("register");
 
-    // handler you're logged or not?
-    {
-      if (!getAccount?.length && !isLoggedPage) {
-        window.location.pathname = `${utilsConstants.BASE_PATH}/login`;
-      }
-
-      if (getAccount?.length && isLoggedPage) {
-        window.location.pathname = `${utilsConstants.BASE_PATH}/`;
-      }
+    // you're not logged but tried to go home
+    if (!getAccessToken && !isLoggedPage) {
+      window.location.pathname = `${utilsConstants.BASE_PATH}/login`;
     }
 
-    if (getAccount?.length) {
-      // context = await fetchAccount(getAccount);
-    }
-
-    // handler expires login
-    {
-      // ... E.g
+    // you're logged but tried to go login page
+    if (getAccessToken && isLoggedPage) {
+      window.location.pathname = `${utilsConstants.BASE_PATH}/`;
     }
   })()
 );
--- utils/index.js
+++ utils/index.js
@@ -1,3 +1,5 @@
+import { Buffer } from "buffer";
+
 // Record<string, boolean>
 export const catchProperties = (obj) => {
   for (const [key, conditional] of Object.entries(obj)) {
@@ -7,4 +9,14 @@ export const catchProperties = (obj) => {
 
 export const RANDOM_CHARACTER = () => {
   return (Math.random() + 1).toString(6).substring(7);
+};
+
+// handler JWT for encode
+export const parseJWT = (token) => {
+  const arr = token?.split(".");
+  const base64Payload = (arr?.length || 0) > 1 ? arr?.[1] : undefined;
+
+  const payload = base64Payload ? Buffer.from(base64Payload, "base64") : "{}";
+
+  return JSON.parse(payload.toString());
 };
